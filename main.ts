f0a87267674bdb9900bd1bea4da1ab308ae4c6ce
Kennedy Mahoro <kennedymahoro1@gmail.com>
2022-06-14 21:52:49 +0000

    added a class NewElement

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
 
 const Main_Content:HTMLDivElement = document.querySelector("data-content") as HTMLDivElement;
+const Input_Book:HTMLInputElement = document.querySelector("data-inputBook") as HTMLInputElement;
+const Input_Author:HTMLInputElement = document.querySelector("data-inputAuthor") as HTMLInputElement;
+const Input_Page:HTMLInputElement = document.querySelector("data-inputPage") as HTMLInputElement;
+const Input_Read_Checkbox:HTMLInputElement = document.querySelector("data-checkbox") as HTMLInputElement;
 
+class NewElement{
+    Author:string;
+    Book:string;
+    Page:number;
+    Read:boolean;
+
+    constructor(){
+        this.Author;
+        this.Book;
+        this.Read;
+        this.Page;
+    }
+    GetElement(){
+        return
+        `<div class="CreatedElement" data-createdElement>
+            <div class="CE_Author" data-CEAuthor>${this.Author}</div>
+            <div class="CE_Book" data-CEBook>${this.Book}</div>
+            <div class="CE_Page" data-CEPage>${this.Page}</div>
+            <input type="checkbox" class="CE_Read" data-CERead>${this.Read} pages</input>
+            <button class="CE_Button" data-Delete>Delete</button>
+        </div>
+        
+        `
+    }
+}
